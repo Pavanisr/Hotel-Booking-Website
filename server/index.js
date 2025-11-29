32fa22ba3f26b83a0d4a9c85578ff92c04bbd224
Pavanisr <pavaniranasinghe425@gmail.com>
2025-11-29 15:33:28 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const pool = require("./models/db"); // PostgreSQL connection
+
+const authRoutes = require("./routes/auth");
+const hotelRoutes = require("./routes/hotels");
+const bookingRoutes = require("./routes/bookings");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Test route
-app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.send(`Server is running 🚀 Database time: ${result.rows[0].now}`);
-  } catch (err) {
-    res.status(500).send("Database error: " + err.message);
-  }
-});
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/hotels", hotelRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
