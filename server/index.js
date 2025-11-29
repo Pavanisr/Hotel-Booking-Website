6dd2ca5963ee9bfaf38d92eb5818bb0f7daba82c
Pavanisr <pavaniranasinghe425@gmail.com>
2025-11-29 14:07:53 +0530

    change the db.js and index.js for check the server and db connection

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
-const authRoutes = require("./routes/auth");
-const hotelRoutes = require("./routes/hotels");
-const bookingRoutes = require("./routes/bookings");
+const pool = require("./models/db"); // PostgreSQL connection
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/hotels", hotelRoutes);
-app.use("/api/bookings", bookingRoutes);
+// Test route
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(`Server is running 🚀 Database time: ${result.rows[0].now}`);
+  } catch (err) {
+    res.status(500).send("Database error: " + err.message);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
