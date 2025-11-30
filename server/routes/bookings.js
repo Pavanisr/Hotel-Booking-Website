fd75f0d91f4c87d22b3af79ca4fe901537bb6345
Pavanisr <pavaniranasinghe425@gmail.com>
2025-11-30 12:27:01 +0530

    update booking.js

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { createBooking, getUserBookings } = require("../controllers/bookingsController");
+const { 
+  createBooking, 
+  getUserBookings, 
+  getBookingById, 
+  cancelBooking 
+} = require("../controllers/bookingsController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/create", authMiddleware, createBooking);
 router.get("/user", authMiddleware, getUserBookings);
+
+// New ones
+router.get("/:id", authMiddleware, getBookingById);
+router.delete("/:id", authMiddleware, cancelBooking);
 
 module.exports = router;
